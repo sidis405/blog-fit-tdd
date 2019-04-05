dce6c71330e3e859b57fac878eed2d295e3a09dd
Sidrit Trandafili <forge405@gmail.com>
2019-04-05 15:18:25 +0200

    test api post show

--- tests/Feature/Api/PostShowingTest.php
+++ tests/Feature/Api/PostShowingTest.php
@@ -3,20 +3,37 @@
 namespace Tests\Feature\Api;
 
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithFaker;
+use Acme\Models\Post;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class PostShowingTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function testExample()
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    /** @test */
+    public function user_can_see_a_post()
+    {
+        // arrange
+        $post = create(Post::class);
+
+        // act
+        $response = $this->get(route('api.posts.show', $post));
+
+        // assert
+        $response->assertHeader('content-type', 'application/json');
+        $response->assertJsonStructure([
+            'data' => []
+        ]);
+
+        $response->assertJsonFragment([
+            'id' => $post->id,
+            'title' => $post->title,
+        ]);
+
+        $response->assertJsonStructure([
+            'data' => [
+                'title', 'slug', 'category', 'user'
+            ],
+        ]);
     }
 }
